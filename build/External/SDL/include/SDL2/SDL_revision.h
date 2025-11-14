4fa1479a5ec2c1fa15843097425567f12a0f5752
NDXDirectorsCut <Rares_sincu@yahoo.com>
2025-11-14 08:57:26 +0200

    11.11.25

---
+++ build/External/SDL/include/SDL2/SDL_revision.h
@@ -0,0 +1,8 @@
+/* #undef SDL_VENDOR_INFO */
+#define SDL_REVISION_NUMBER 0
+
+#ifdef SDL_VENDOR_INFO
+#define SDL_REVISION "SDL-release-2.32.0-130-g6db23faa4 (" SDL_VENDOR_INFO ")"
+#else
+#define SDL_REVISION "SDL-release-2.32.0-130-g6db23faa4"
+#endif
