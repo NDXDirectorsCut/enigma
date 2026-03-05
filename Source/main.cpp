01ab05e325b8c79199dbbcc8cc954d16229bdb60
NDXDirectorsCut <Rares_sincu@yahoo.com>
2026-03-05 23:56:22 +0200

    ditto 1

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
-#include "Application/Application.hpp"
-
 int n;
 
 int main(int argc, char* argv[])
 { 
-    Application application;
-    if(!application.Start())
-    {
-        std::cout<<"Initialization Failed! \n";
-        return 1;
-    }
+    // Application application;
+    // if(!application.Start())
+    // {
+    //     std::cout<<"Initialization Failed! \n";
+    //     return 1;
+    // }
 
-    while(application.IsRunning())
-    {
-        application.Update();
-        application.FixedUpdate();
-    }
+    // while(application.IsRunning())
+    // {
+    //     application.Update();
+    //     application.FixedUpdate();
+    // }
 
     return 0;
 }
